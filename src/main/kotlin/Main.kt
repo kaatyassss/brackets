14622cca8d6eb4f27a5d0878427ab7885201aaac
Katya <salnikovaea.21@edu.ystu.ru>
2021-12-09 22:57:46 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,44 @@
+/** Задача на 3 вида скобок **/
+fun main(){
+    val s:String = readLine().toString()
+    print(check(s))
+}
+
+fun check(s:String): String {
+    val st = mutableListOf<Char>() // Создаем стек со скобками
+    val per = "(){}[]"
+    for (i in s.indices) {
+        val index = per.indexOf(s[i])
+        //println(st)
+        //println(s[i])
+        if (index >= 0) {
+            if (index % 2 == 1) { // Закрывающая скобка
+                if (st.size == 0) return "NO" // Если стек пустой, значит расстановка неправильная
+                val last = st.lastOrNull() // Берем последний элемент из стека
+                st.removeLastOrNull() // После того, как взяли элемент, сразу удаляем его из стека
+                if (last != per[index - 1]) {
+                    return "NO" // Если последний элемент из стека не будет соотвествовать текущему, то расстановка неправильная
+                }
+            } else st.add(s[i])
+        } else return "NO"
+    }
+    return "YES"
+}
+
+/** Задача на простые круглые скобки **/
+/*fun main() {
+    val s: String = readLine().toString()
+    print(check(s))
+}
+
+fun check(s: String): String {
+    var k = 0
+    for (i in s){
+        if (i == '('){
+            k += 1;
+        } else k -= 1
+    }
+    if (k == 0){
+        return "YES"
+    } else return "NO"
+}*/
