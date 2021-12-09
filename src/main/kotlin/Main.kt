cecc96c47d290d09cf2dd68b713429b4dbf24e57
Katya <salnikovaea.21@edu.ystu.ru>
2021-12-09 22:58:28 +0300

    Initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,19 +5,17 @@ fun main(){
 }
 
 fun check(s:String): String {
-    val st = mutableListOf<Char>() // Создаем стек со скобками
+    val st = mutableListOf<Char>() // Создаем стек для скобок
     val per = "(){}[]"
     for (i in s.indices) {
         val index = per.indexOf(s[i])
-        //println(st)
-        //println(s[i])
         if (index >= 0) {
-            if (index % 2 == 1) { // Закрывающая скобка
+            if (index % 2 == 1) { // Если закрывающая скобка
                 if (st.size == 0) return "NO" // Если стек пустой, значит расстановка неправильная
                 val last = st.lastOrNull() // Берем последний элемент из стека
                 st.removeLastOrNull() // После того, как взяли элемент, сразу удаляем его из стека
                 if (last != per[index - 1]) {
-                    return "NO" // Если последний элемент из стека не будет соотвествовать текущему, то расстановка неправильная
+                    return "NO" // Если последний элемент из стека не будет соответствовать текущему, то расстановка неправильная
                 }
             } else st.add(s[i])
         } else return "NO"
